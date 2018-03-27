ba0616142c9969e3d1dd3a89b65771f23186a52e
GA-MO <ga-mo@GA-MOs-MacBook-Pro.local>
2018-03-27 10:30:34 +0700

    Update prop maxWidth for Container

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export default class Container extends Component {
+  static propTypes = {
+    maxWidth: PropTypes.number
+  }
+
   render () {
+    const { maxWidth } = this.props
+    const styleContainer = maxWidth ? { maxWidth: `${maxWidth}px` } : {}
+
     return (
-      <div className='container'>
+      <div className='container' style={styleContainer}>
         {this.props.children}
       </div>
     )
