e5e13462be8ac2b49d0df2faf1605ddb63631460
Thawatchai Khuansombat <thawatchai.deartk@gmail.com>
2019-10-03 14:33:25 +0700

    add data-modalchild

--- src/components/useModalOpenInMobileLayout.js
+++ src/components/useModalOpenInMobileLayout.js
@@ -28,10 +28,29 @@ const getParentClosest = (elem, selector) => {
 export default (ref, show) => {
   useEffect(() => {
     const parent = getParentClosest(ref.current, '#ui-container')
+
+
     if (parent && show) {
+      const modalChild = parent.getAttribute('data-modalchild')
+      if (modalChild) {
+        parent.setAttribute('data-modalchild', `${Number(modalChild) + 1}`)
+      } else {
+        parent.setAttribute('data-modalchild', '1')
+      }
       parent.style.overflow = 'hidden'
     }
 
-    return () => { if (parent && show) { parent.style.overflow = 'auto' } }
+    return () => {
+      if (parent && show) {
+        const modalChild = parent.getAttribute('data-modalchild')
+        if (modalChild === '1') {
+          parent.style.overflow = 'auto'
+          parent.setAttribute('data-modalchild', '0')
+        } else {
+          const newModalChild = Number(modalChild) - 1
+          parent.setAttribute('data-modalchild', `${newModalChild}`)
+        }
+      }
+    }
   }, [show])
 }
